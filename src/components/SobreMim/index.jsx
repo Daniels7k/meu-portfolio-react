1a1ea74e5ae3456fd35240d35b8159baaba3c292
Cleiton Daniel (Kaonami) <danielsdev77@gmail.com>
2022-06-06 10:46:30 -0300

    Update index.jsx

--- src/components/SobreMim/index.jsx
+++ src/components/SobreMim/index.jsx
@@ -7,10 +7,8 @@ function SobreMim() {
             <div className="sobreMim-Box">
                 <span className="sobreMim"> Sobre Mim</span>
                 <p className="conteudo-sobreMim">
-                    Desenvolvo aplicações em FullStack a mais de 1 ano, utilizando tecnologias como JavaScript, HTML CSS, Node.JS, Express.js, MongoDB, Mysql e outras bibliotecas,
-                    já colaborei também com traduções do inglês para o português em projetos open-source no github.
-                    Possuo flexibilidade para aprender em pouco tempo qualquer outra tecnologia que me for dada. 
-                    Dê uma olhada em meus trabalhos no meu repositório no Gibhub, eles são bem elaborados, comentados e possuem imagens para facilitar a visualização.
+                    Possuo experiência com desenvolvimento FullStack utilizando, Node.js e React.js, já trabalhei no back-end com criações de API REST utilizando Express.js, banco de dados não relacionais como MongoDB e relacionais como o MySQL.
+                    No front-end tenho conhecimento em React.js com compentização, integrações de API, API Context, validações e desenvolvimento de layouts com HTML e CSS.
                 </p>
                 <div className="sobreMim-buttons">
                     <a href="#meusprojetos-h1" rel="noopener"><button>Meus Projetos</button></a>
